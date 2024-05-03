794c0f7b059ef25d1872cb769bdc074cf33eb6ad
MD MOZAMMIL <104304149+MuzzammiI@users.noreply.github.com>
2024-05-03 23:45:50 +0530

    Day-8 Challenge Completed Create a countdown clock

---
+++ Day-1-10/Day-8/index.js
@@ -0,0 +1,18 @@
+let date =  new Date();
+let todayDate = date.getDate();
+let year = date.getFullYear();
+let month =  date.getMonth();
+let day = date.getDay();
+let hours=date.getHours();
+let min = date.getMinutes();
+let secs = date.getSeconds();
+
+let setDate = 
+
+console.log(todayDate,month,day,hours,min,secs,year)
+
+
+
+setInterval(()=>{
+
+},setDate)
