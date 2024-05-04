4e3b090bda34ff244d46e166b8e4adfdaadbfa05
MD MOZAMMIL <104304149+MuzzammiI@users.noreply.github.com>
2024-05-04 23:49:29 +0530

    Js Added

--- Day-1-10/Day-8/index.js
+++ Day-1-10/Day-8/index.js
@@ -1,18 +1,13 @@
-let date =  new Date();
-let todayDate = date.getDate();
-let year = date.getFullYear();
-let month =  date.getMonth();
-let day = date.getDay();
-let hours=date.getHours();
-let min = date.getMinutes();
-let secs = date.getSeconds();
-
-let setDate = 
-
-console.log(todayDate,month,day,hours,min,secs,year)
 
 
+var Month = [
+    "January",
+    "February","March","April","May","June","July","August","September","October","November","December"
+]
+var Weeks =[
+    "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"
+];
 
-setInterval(()=>{
+const date = new Date(2024,4,1,13,30,0)
 
-},setDate)
+
