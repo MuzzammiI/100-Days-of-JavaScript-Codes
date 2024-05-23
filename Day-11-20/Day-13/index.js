d609cc69c76bcbbb3d98d803d9cc052b4dffd127
MD MOZAMMIL <104304149+MuzzammiI@users.noreply.github.com>
2024-05-23 22:07:00 +0530

    Update Js

--- Day-11-20/Day-13/index.js
+++ Day-11-20/Day-13/index.js
@@ -1,29 +1,45 @@
 
-let SelectItem = document.querySelectorAll("#QuizForm div");
 const submitBtn = document.getElementById("submit");
+const answer = "Narendra Modi";
+const popitem = document.querySelector(".popup")
+const correct = document.getElementById("correct")
+const wrong = document.getElementById("wrong")
 
 
 
 
+submitBtn.addEventListener("click",UserData)
+
+function UserData(e){
+e.preventDefault();
+let SelectItem = document.querySelectorAll("#QuizForm div");
 
 
-submitBtn.addEventListener("click",function(e){
-    e.preventDefault();
-    UserData();
-})
-
-
-function UserData(){
-
-    const ans = "Narendra Modi"
     SelectItem.forEach((item)=>{
         item.addEventListener("click",function(data){
             let finalData = data.target.value;
-            if(finalData){
-                console.log("hello")
+            if(answer===finalData){
+                popitem.classList.add("show")
+                correct.classList.add("show")
+                wrong.classList.remove("show");
+                durationTimme();
             }
-            
+            else{
+                popitem.classList.add("show")
+                correct.classList.remove("show");
+                wrong.classList.add("show")
+                durationTimme();
+            }
         })
     })
+    
 }
 
+
+
+const durationTimme = ()=>{
+setTimeout(()=>{
+    popitem.classList.remove("show");
+},2000)
+
+}
